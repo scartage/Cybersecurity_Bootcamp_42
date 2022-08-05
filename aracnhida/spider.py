d4ad042eb1e6b4f5c3629575e466fcbce7eab342
scartage <scartage_estudios@proton.me>
2022-08-05 16:13:51 +0200

    parametros hechos.
    El programa funciona con los parametros pedidos de una forma muy basica. Falta implementar el argparser y revisar el comportamiento en diferentes paginas, en teoria ya esta terminado el spider.

--- aracnhida/spider.py
+++ aracnhida/spider.py
@@ -55,42 +55,51 @@ def download_image(download_path, img, file_name):
         print(f"FAILED: for this reason {e} - ")
 
 
-def get_url_img(url):
-    image_url_local = []
-    image_to_down_local = []
+def get_url_img(url, path):
+    image_url_RE = []
+    image_to_down_RE = []
    
 
     get_html_data = getdata(url)
     sopa = BeautifulSoup(get_html_data, 'html.parser')
 
     for item in sopa.find_all('img'):
-        image_url_local.append(item['src'])
+        image_url_RE.append(item['src'])
 
-    for img in image_url_local:
+    for img in image_url_RE:
         name, ext = os.path.splitext(img)
         if ext in ext_to_download:
-            image_to_down_local.append(img)         #estas son las imagenes que se descargan por recursividad
+            image_to_down_RE.append(img)         #estas son las imagenes que se descargan por recursividad
         else:
-            continue 
-    for img in image_to_down_local:
-        download_image("./data/", img, "testRE")
-
+            continue
+    if path == "./data":
+        for img in image_to_down_RE:
+            download_image("./data", img, "spiderRE")
+    else:
+        for img in image_to_down_RE:
+            download_image(path, img, "spiderRE")
 
 def make_path(path):
-    if not os.path.exists(path):
-        os.mkdir(path)
-        os.mkdir("data/jpg-jpeg/")
-        os.mkdir("data/png/")
-        os.mkdir("data/gif/")
-        os.mkdir("data/bmp/")
+    if path == "./data":
+        if not os.path.exists(path):
+            os.mkdir(path)
+            os.mkdir("data/jpg-jpeg/")
+            os.mkdir("data/png/")
+            os.mkdir("data/gif/")
+            os.mkdir("data/bmp/")
     else:
-        return
+        if not os.path.exists(path):
+            os.mkdir(path)
+            os.mkdir(path + "/jpg-jpeg/")
+            os.mkdir(path + "/png/")
+            os.mkdir(path + "/gif/")
+            os.mkdir(path + "/bmp/")
+
 
     
 def principal(path, url, parametro, cantidad):
     htmldata = getdata(url)
     soup = BeautifulSoup(htmldata, 'html.parser')
-    
     make_path(path)
 
     for item in soup.find_all('a'):
@@ -105,20 +114,18 @@ def principal(path, url, parametro, cantidad):
 
     if parametro == True:                       #aqui enviamos a recursividad
         if cantidad == 0:
-            i = 0
+            i = 1
             for link in recursive_links:
-                get_url_img(link)
-                if i == 5:
+                get_url_img(link, path)
+                if i == 6:
                     break
-                print(i)
                 i += 1
         else:
-            i = 0
+            i = 1
             for link in recursive_links:
-                if i == cantidad:
+                if i == cantidad + 1:
                     break
-                get_url_img(link)
-                print(i)
+                get_url_img(link, path)
                 i += 1
     else:
         for item in soup.find_all('img'):
@@ -130,27 +137,49 @@ def principal(path, url, parametro, cantidad):
                 image_to_down.append(img)           
             else:
                 continue
-        for img in image_to_down:
-            download_image("./data/", img, "test")
+        if path == "./data":
+            for img in image_to_down:
+                download_image("./data/", img, "spider")
+        else:
+            for img in image_to_down:
+                download_image(path, img, "spider")
 
 
     
 if __name__ == "__main__":
     args = sys.argv[1:]
     len_p = len(args)
+    print(len_p)
     if len_p == 1:
         url_to_look = args[0]
         principal("./data", url_to_look, False, 0)
     elif len_p == 2:
         if "-r" in args:
-            print("im in")
             url_to_look = args[1]
             principal ("./data", url_to_look, True, 0)
+    elif len_p == 3:
+        if "-p" in args:
+            path = args[1]
+            url = args[2]
+            print(f"path: {path}, url: {url}")
+            principal(path, url, False, 0)
     elif len_p == 4:
         if "-r" in args and "-l" in args:
             cantidad = int(args[2])
             url = args[3]
             principal("./data", url, True, cantidad)
+        elif "-r" in args and "-p" in args:
+            path = args[2]
+            url = args[3]
+            print(f"este es el path {path} y la url {url}")
+            principal(path, url, True, 0)
+    elif len_p == 6:
+        if "-r" in args and "-l" in args and "-p" in args:
+            cantidad = int(args[2])
+            path = args[4]
+            url = args[5]
+            print(f"cantidad :{cantidad}, ubi: {path}, url: {url}")
+            principal(path, url, True, cantidad)
     else:
         print("Error: Parametros incorrectos")
     
